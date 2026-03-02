509484384f394193589c5565c1161526fcb10464
plasmaDestroyer <satyasheel2004@gmail.com>
2026-03-02 16:02:50 +0530

    feat: AST parser with operator precedence and tree-walking evaluator

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 #[derive(Debug)]
 enum Token {
     Number(i64),
-    Operator(Op)
+    Operator(Op),
 }
 
 #[derive(Debug)]
+enum Node {
+    Number(i64),
+    Operator(Op, Box<Node>, Box<Node>),
+}
+
+#[derive(Debug, Clone, Copy)]
 enum Op {
     Plus,
     Minus,
     Multiply,
-    Divide
+    Divide,
 }
 
 fn main() {
@@ -29,27 +35,95 @@ fn main() {
                 "-" => tokens.push(Token::Operator(Op::Minus)),
                 "*" => tokens.push(Token::Operator(Op::Multiply)),
                 "/" => tokens.push(Token::Operator(Op::Divide)),
-                _ => ()
+                _ => (),
             }
         }
     }
 
     println!("Tokens: {:?}", tokens);
 
-    let mut res = match tokens[0] { Token::Number(num) => num, _ => 0 };
+    let root = parse_low(&tokens, &mut 0);
 
-    for i in (1..tokens.len()).step_by(2) {
-        match (&tokens[i], &tokens[i + 1]) {
-            (Token::Operator(op), Token::Number(num)) => match op {
-                Op::Plus => res += num,
-                Op::Minus => res -= num,
-                Op::Multiply => res *= num,
-                Op::Divide => res /= num,
+    println!("Tree: {:?}", root);
+
+    let res: i64 = evaluate(root);
+
+    println!("Answer: {:?}", res);
+}
+
+fn parse_low(tokens: &[Token], pos: &mut usize) -> Node {
+    
+    let mut left: Node = parse_high(tokens, pos);
+
+    loop {
+        if *pos >= tokens.len() { break; }
+
+        let Token::Operator(op) = tokens[*pos] else { break; }; 
+        
+        match op {
+            Op::Plus | Op::Minus => {
+                *pos += 1;
+                let right: Node = parse_high(tokens, pos);
+                left = Node::Operator(op, Box::new(left), Box::new(right));
             },
-            _ => ()
+            _ => break
         }
     }
 
-    println!("Answer: {:?}", res);
+    left
+}
 
+fn parse_high(tokens: &[Token], pos: &mut usize) -> Node {
+    
+    let mut left: Node = parse_number(tokens, pos);
+
+    loop {
+        if *pos >= tokens.len() { break; }
+
+        let Token::Operator(op) = tokens[*pos] else { break; }; 
+        
+        match op {
+            Op::Multiply | Op::Divide => {
+                *pos += 1;
+                let right: Node = parse_number(tokens, pos);
+                left = Node::Operator(op, Box::new(left), Box::new(right));
+            },
+            _ => break
+        }
+    }
+
+    left
+}
+
+fn parse_number(tokens: &[Token], pos: &mut usize) -> Node {
+    
+    match tokens[*pos] {
+        Token::Number(num) => {
+            *pos += 1;
+            Node::Number(num)
+        },
+        _ => todo!()
+    }
+
+}
+
+fn evaluate(node: Node) -> i64 {
+
+    let res: i64;
+
+    match node {
+        Node::Number(num) => res = num,
+        Node::Operator(op, left, right) => {
+            let left_num = evaluate(*left);
+            let right_num = evaluate(*right);
+            match op {
+                Op::Plus => res = left_num + right_num,
+                Op::Minus => res = left_num - right_num,
+                Op::Multiply => res = left_num * right_num,
+                Op::Divide => res = left_num / right_num
+            }
+        }
+    }
+
+    res
 }
